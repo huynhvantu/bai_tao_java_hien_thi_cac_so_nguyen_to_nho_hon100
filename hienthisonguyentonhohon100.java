f468607d206818f7a70b7fec73067e9568f54131
huynhvantu <huynhvantunlp@gmail.com>
2019-06-22 18:03:20 +0700

    first commit

---
+++ hienthisonguyentonhohon100.java
@@ -0,0 +1,29 @@
+package hien_thi_cac_so_nguyen_to_nho_hon_100;
+
+public class hienthisonguyentonhohon100 {
+   
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố nhỏ hơn 100 là: ");
+        for (int i = 0; i < 100; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+  
+    
+    public static boolean isPrimeNumber(int n) {
+        // so nguyen n < 2 khong phai la so nguyen to
+        if (n < 2) {
+            return false;
+        }
+        // check so nguyen to khi n >= 2
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
